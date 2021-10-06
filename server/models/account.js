686ae0d5bb27ebac5ec2640d394aa65d9412b7ec
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-10-06 14:55:18 +0100

    Added schema, installed test frameworks

---
+++ server/models/account.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const options = { discriminatorKey: 'kind' };
+
+const accountSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    address: {
+      streetAddress: { type: String, required: true },
+      city: { type: String, required: true },
+      postCode: { type: String, required: true },
+    },
+    telephoneNumber: { type: Number, required: true },
+    description: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+  options,
+);
+
+const Charity = Account.discriminator(
+  'Charity',
+  new mongoose.Schema({
+    charityNumber: { type: Number, required: true },
+    websiteLink: { type: String, required: true },
+    options,
+  }),
+);
+
+const Restaurant = Account.discriminator(
+  'Restaurant',
+  new mongoose.Schema({
+    foodHygieneRating: { type: String, required: true },
+    options,
+  }),
+);
+
+const Account = mongoose.model('Account', accountSchema);
---
+++ server/models/listing.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const listingSchema = new mongoose.Schema(
+  {
+    numberOfMeals: { type: Number, required: true },
+    description: { type: String, required: true },
+    timeAvailableUntil: { type: Date, required: true },
+    listedBy: { type: Number, required: true },
+    claimedBy: { type: Number },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Listing = mongoose.model('Listing', listingSchema);
