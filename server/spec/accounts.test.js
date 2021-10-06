7e490c93e603c201fcc8f6f2177c0987ec52980a
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-10-06 18:40:31 +0100

    Jest tests passing

--- server/spec/accounts.test.js
+++ server/spec/accounts.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from '../server.js';
 
 describe('POST /users', () => {
-  describe('when passed a username and password', () => {
+  describe('when passed all required fields', () => {
     test('should respond with a 200 status code', async () => {
       const response = await request(app)
         .post('/accounts/create')
@@ -23,6 +23,39 @@ describe('POST /users', () => {
           foodHygieneRating: 'beau',
         });
       expect(response.statusCode).toBe(200);
+    });
+  });
+
+  describe('when not passed all required fields', () => {
+    test('should respond with a 400 status code', async () => {
+      const response = await request(app).post('/accounts/create').send({
+        type: 'charity',
+      });
+      expect(response.statusCode).toBe(400);
+    });
+  });
+
+  describe('when validation is not satisfied', () => {
+    test('should respond with a 400 status code', async () => {
+      const response = await request(app)
+        .post('/accounts/create')
+        .send({
+          type: 'charity',
+          name: 'beau2',
+          email: 'beau',
+          password: 'beau',
+          address: {
+            streetAddress: 'beau',
+            city: 'beau',
+            postCode: 'beau',
+          },
+          telephoneNumber: 'beau',
+          description: 'beau',
+          charityNumber: 'beau',
+          websiteLink: 'beau',
+          foodHygieneRating: 'beau',
+        });
+      expect(response.statusCode).toBe(400);
     });
   });
 });
