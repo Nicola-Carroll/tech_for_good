e7c65a5f8a270a0c3df927e9d7c51c1022418f0d
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-10-06 21:05:00 +0100

    refactor to async functn

--- server/controllers/listings.js
+++ server/controllers/listings.js
@@ -1,17 +1,16 @@
 import Listing from '../models/listing.js';
 
 const ListingsController = {
-  Index(req, res) {
-    // res.send('Hello Listings page!');
-
-    Listing.find()
-      .then((listings) => res.json(listings))
-      .catch((error) => res.status(400).json(`Error: ${error}`));
+  async Index(req, res) {
+    try {
+      const listings = await Listing.find();
+      const data = await res.json(listings);
+    } catch (error) {
+      res.status(400).json(`Error: ${error}`);
+    }
   },
 
   async New(req, res) {},
 };
-
-// module.exports = ListingsController;
 
 export default ListingsController;
