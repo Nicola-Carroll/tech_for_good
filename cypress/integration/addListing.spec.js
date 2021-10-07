13c91c2f5f141a205a9f45a2f10eb5fcfbcaeb68
zainab baruud <zainabbaruud@ma373.home>
2021-10-07 17:20:07 +0100

    Adding feature test

--- cypress/integration/addListing.spec.js
+++ cypress/integration/addListing.spec.js
@@ -5,4 +5,12 @@ describe('Add Listing', () => {
     cy.contains('Description');
     cy.contains('Create Listing');
   });
+
+  it('checks click', () => {
+    cy.visit('http://localhost:3000/listings/new');
+    cy.get('Number of Meals').type('10')
+    cy.get('Description').type('Hello, World')
+    cy.get('.btn').click()
+    cy.focused().click()
+  });
 });
