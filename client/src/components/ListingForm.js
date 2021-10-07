4696f7b2f6927cea2196bc4fe68dfba777b58ee1
eballantine <lizzy.kayballantine@gmail.com>
2021-10-07 16:44:28 +0100

    WIP swap to ZB

--- client/src/components/ListingForm.js
+++ client/src/components/ListingForm.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 
-export default function ListingForm() {
+export default function listingForm() {
   function handleSubmit(e) {
     e.preventDefault();
+
+    axios
+      .post('http://localhost:3000/listings/new', {
+        numberOfMeals: '',
+        description: '',
+        timeAvailableUntil: '',
+        listedBy: '',
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
   }
+
   return (
     <section className="section w-50 m-auto">
       <form className="listing-form" onSubmit={handleSubmit}>
@@ -24,15 +37,22 @@ export default function ListingForm() {
           <label className="mt-2 mb-2" htmlFor="desc">
             Description
           </label>
-          <textarea className="form-control" type="text" name="desc" id="desc" rows="3"></textarea>
+          <textarea
+            className="form-control"
+            type="text"
+            name="desc"
+            id="desc"
+            rows="3"
+          ></textarea>
           <small id="passwordHelpBlock" class="form-text text-muted">
-            Please describe the type of food along with the dietary requirements.
-            </small>
+            Please describe the type of food along with the dietary
+            requirements.
+          </small>
         </div>
         <div className="form-submit">
-          <button
-            className="btn btn-outline-success mt-3"
-            type="submit">Create Listing</button>
+          <button className="btn btn-outline-success mt-3" type="submit">
+            Create Listing
+          </button>
         </div>
       </form>
     </section>
