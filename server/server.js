2b1602d1052185ab28e523904b12402acd037af9
Paul Rainsford <paulrainsford69@gmail.co>
2021-10-07 17:58:33 +0100

    Added dropdatatbase to server.js file

--- server/server.js
+++ server/server.js
@@ -29,5 +29,5 @@ const PORT = process.env.PORT || 8000;
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
-
+else { mongoose.connection.dropDatabase() }
 export default app;
