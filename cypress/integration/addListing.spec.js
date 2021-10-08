0a33b050eeda9717efddfef4b926c62c1423b97f
eballantine <lizzy.kayballantine@gmail.com>
2021-10-08 12:21:07 +0100

    Delete cypress from root

--- cypress/integration/addListing.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Add Listing', () => {
-  it('contains new listing form', () => {
-    cy.visit('http://localhost:3000/listings/new');
-    cy.contains('Number of Meals');
-    cy.contains('Description');
-    cy.contains('Create Listing');
-  });
-});
