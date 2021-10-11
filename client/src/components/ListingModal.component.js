735795ace27b3c17f95e071c80b72fa7b7675178
eballantine <lizzy.kayballantine@gmail.com>
2021-10-11 16:45:37 +0100

    Some modal styling

--- client/src/components/ListingModal.component.js
+++ client/src/components/ListingModal.component.js
@@ -27,15 +27,32 @@ export default class ListingModal extends Component {
       <Modal
         isOpen={!!this.props.listingId}
         contentLabel="Minimal Modal Example"
+        style={{
+          content: {
+            top: '10%',
+            left: '20%',
+            right: '20%',
+            bottom: '60%',
+            background: '#39C0ED',
+          },
+        }}
       >
+        <h3 className="m-3 mb-4">
+          Are you sure you want to claim this listing?
+        </h3>
         <button
+          className="btn btn-secondary mx-3"
+          onClick={this.props.handleClose}
+          id="back-button"
+        >
+          Go back
+        </button>
+        <button
+          className="btn btn-primary mx-1"
           onClick={() => this.claimListing(this.props.listingId)}
           id="claim-button"
         >
           Confirm
-        </button>
-        <button onClick={this.props.handleClose} id="back-button">
-          Go back
         </button>
       </Modal>
     );
