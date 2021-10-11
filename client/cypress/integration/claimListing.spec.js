0f9a48a49fb0f16dd6a099473eec51f89eaedf38
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-10-11 11:55:23 +0100

    claim listing modal feature tests

--- client/cypress/integration/claimListing.spec.js
+++ client/cypress/integration/claimListing.spec.js
@@ -2,26 +2,42 @@
 import axios from 'axios';
 
 describe('Claiming a listing', () => {
-  // not sure how we will click the claim listing button - will each button have its own id or will the id be fed
-  // as an arg to the onClick event
-
-  // it('claiming a listing takes the user to a new page', () => {
-  //   cy.visit('/feed');
-  //   cy.get('.claim-listing').click()
-  //   cy.contains('This listing has been claimed, please come to collect within an hour');
-  // });
-
-  // this test can't find the claim button :((
-
   it('can claim a listing', () => {
     cy.addListing(10, 'Test listing to be claimed');
 
     cy.visit('/feed');
     cy.get('.btn').first().click();
+    cy.get('#claim-button').click();
     cy.visit('/feed');
     cy.get('.desc')
       .first()
       .contains('Test listing to be claimed')
       .should('not.exist');
+  });
+
+  it('allows the user to confirm', () => {
+    cy.addListing(10, 'Test listing to be claimed');
+
+    cy.visit('/feed');
+    cy.get('.btn').first().click();
+    cy.contains('Confirm');
+  });
+
+  it('allows the user to go back', () => {
+    cy.addListing(10, 'Test listing to be claimed');
+
+    cy.visit('/feed');
+    cy.get('.btn').first().click();
+    cy.contains('Go back');
+  });
+
+  it('go back returns to feed', () => {
+    cy.addListing(10, 'Test listing to be claimed');
+
+    cy.visit('/feed');
+    cy.get('.btn').first().click();
+    cy.get('#back-button').click();
+    cy.get('.desc').first().contains('Test listing to be claimed');
+    cy.get('#back-button').should('not.exist');
   });
 });
