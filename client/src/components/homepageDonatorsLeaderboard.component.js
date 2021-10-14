6370703073d6ad89d18a52fa77374b7f297343cb
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-10-14 20:02:34 +0100

    Final css adjustments

--- client/src/components/homepageDonatorsLeaderboard.component.js
+++ client/src/components/homepageDonatorsLeaderboard.component.js
@@ -40,7 +40,9 @@ export default class Leaderboard extends Component {
       return (
         <>
           <section id="top-donators">
-            <h2 className="text-center display-5 pt-5">Most food donated</h2>
+            <h2 className="text-center display-5 pt-5 pb-5">
+              Most food donated
+            </h2>
             <table id="top-donators" className="table w-50 mx-auto">
               <thead className="thead-dark">
                 <tr>
