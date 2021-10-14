055e079cce7f956b0b3630e06fa3ccdfdb16b41f
Paul Rainsford <paulrainsford69@gmail.co>
2021-10-14 12:26:06 +0100

    Changing homepage layout

--- client/src/components/aboutUs.component.js
+++ client/src/components/aboutUs.component.js
@@ -35,15 +35,12 @@ export default class AboutUs extends Component {
   render() {
     return (
       <div className="section w-50 m-auto">
-        <h2 className="mt-2 mb-2 text-center" htmlFor="meals">
-          Who we are:
-        </h2>
-        {/* <p
-          id="totalClaimed"
-          className="row font-weight-bold justify-content-center text-success display-5"
-        >
-          {this.state.total}
-        </p> */}
+        <h1 className="mt-2 mb-2 display-5 text-center" htmlFor="meals">
+        What we do:
+        </h1>
+        <p className="row font-weight-bold justify-content-center text-success display-6 text-center">
+        SecondHelpings is a London based charity food redistributor. We connect businesses that have surplus food with frontline charities. Helping others and reducing waste.
+        </p> 
       </div>
     );
   };
