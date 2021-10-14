b4a9c9943a44d1a67d7d9e2be7badce6a6b01147
eballantine <lizzy.kayballantine@gmail.com>
2021-10-14 11:04:02 +0100

    minor changes to listings card margins

--- client/src/components/listings-feed/listing.component.js
+++ client/src/components/listings-feed/listing.component.js
@@ -3,28 +3,24 @@ import InitiateClaimButton from './InitiateClaimButton.component';
 
 function Listing(props) {
   return (
-    <div className="card m-4 w-75 mx-auto">
-      <div className="card-header">
-        <h5 className="card-title listedBy pt-2">
-          {props.listing.listedByUsername}
-        </h5>
-      </div>
+    <div className="card mt-4 mb-4 w-75 mx-auto p-2">
       <div className="card-body">
-        <p className="card-text meals">
-          <b>Meals available: </b>
-          {props.listing.numberOfMeals}
+        <h5 className="listedBy mb-4">{props.listing.listedByUsername}</h5>
+        <p className="meals">
+          <b>Meals:</b> {props.listing.numberOfMeals}
         </p>
-        <p className="card-text desc">
-          <b>Description: </b>
+        <p className="desc">
+          <b>Description:</b>
+          <br />
           {props.listing.description}
         </p>
-        <p className="card-text time">
-          <b>Available until: </b>
-          {props.listing.timeAvailableUntil}
+        <p className="time">
+          <b>Pick up available until:</b>{' '}
+          {Date(props.listing.timeAvailableUntil)}
         </p>
         <InitiateClaimButton
           listing={props.listing}
-          handleClick={() => props.handleClick(props.listing._id)}
+          handleClick={() => props.handleClick(props.listing)}
         ></InitiateClaimButton>
       </div>
     </div>
